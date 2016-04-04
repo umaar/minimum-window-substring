cd868e39603b370054464d4b3fea59dce3e48cc7
Umar <umar.hansa@gmail.com>
2016-04-04 18:44:09 +0100

    Start implementation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 
-function minimumWindowSubtring() {
-	return 42;
+// "AAAAABBBC", "BAB" === "ABB"
+
+function containsEvery(str, from) {
+	let fromArr = from.split('');
+	return str.split('').every(char => {
+		fromArr.includes(char);
+	});
+}
+
+function minimumWindowSubtring(a, b) {
+	const minimumWindowSize = b.length;
+	for (let i = 0; i < 7; i++) {
+		const windowContents = a.substr(i, minimumWindowSize);
+		console.log('\n:', windowContents, containsEvery(b, windowContents), '\n');
+	}
 }
 
 module.exports = minimumWindowSubtring;
